2d7b8855af268da7efb8d33b61b12fed6e3d1d07
pablom-coder <npmanrique@gmail.com>
2023-01-05 21:04:36 -0300

    Se sube correcciones

--- desafioNro14_clase28/src/controllers/random.controller.js
+++ desafioNro14_clase28/src/controllers/random.controller.js
@@ -1,13 +1,16 @@
+let defaultQuantity = 100000000;
 export const randomNumb = (cant) => {
+  if (cant >= 0) {
+    defaultQuantity = cant;
+  }
+  let numerosRandom = [];
 
-  let numerosRandom;
-
-  for (let i = 0; i < cant; i++) {
-      numerosRandom = Math.floor(Math.random() * cant + 1);
-      console.log(numerosRandom);
+  for (let i = 0; i < defaultQuantity; i++) {
+      numerosRandom.push(Math.floor(Math.random() * 1000));
+      //console.log(numerosRandom);
   }
 
-  return numerosRandom;
+  return numerosRandom.reduce((a, d) => (a[d] ? (a[d] += 1) : (a[d] = 1), a), {});;
 
 };
 
